2346988abf2b84467267454c92e36bc6b9789d9e
Mazin Zakaria <mazinzakaria1@gmail.com>
2015-02-07 14:12:14 -0500

    CatHacks Secondary Design

---
+++ CatHacksDesign/js/parallax.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+	
+	redrawDotNav();
+	
+	/* Scroll event handler */
+    $(window).bind('scroll',function(e){
+    	parallaxScroll();
+		redrawDotNav();
+    });
+    
+	/* Next/prev and primary nav btn click handlers */
+	$('a.main').click(function(){
+    	$('html, body').animate({
+    		scrollTop:0
+    	}, 1000, function() {
+	    	parallaxScroll(); // Callback is required for iOS
+		});
+    	return false;
+	});
+    $('a.signup').click(function(){
+    	$('html, body').animate({
+    		scrollTop:$('#signup').offset().top
+    	}, 1000, function() {
+	    	parallaxScroll(); // Callback is required for iOS
+		});
+    	return false;
+    });
+    $('a.faq').click(function(){
+    	$('html, body').animate({
+    		scrollTop:$('#faq').offset().top
+    	}, 1000, function() {
+	    	parallaxScroll(); // Callback is required for iOS
+		});
+    	return false;
+    });
+	$('a.sponsorpage').click(function(){
+    	$('html, body').animate({
+    		scrollTop:$('#sponsorpage').offset().top
+    	}, 1000, function() {
+	    	parallaxScroll(); // Callback is required for iOS
+		});
+    	return false;
+    });
+    
+    /* Show/hide dot lav labels on hover */
+    $('nav#primary a').hover(
+    	function () {
+			$(this).prev('h1').show();
+		},
+		function () {
+			$(this).prev('h1').hide();
+		}
+    );
+    
+});
+
+/* Scroll the background layers */
+function parallaxScroll(){
+	var scrolled = $(window).scrollTop();
+	$('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
+	$('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
+	$('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
+}
+
+/* Set navigation dots to an active state as the user scrolls */
+function redrawDotNav(){
+	var section1Top =  0;
+	// The top of each section is offset by half the distance to the previous section.
+	var section2Top =  $('#signup').offset().top - (($('#faq').offset().top - $('#signup').offset().top) / 2);
+	var section3Top =  $('#faq').offset().top - (($('#sponsorpage').offset().top - $('#faq').offset().top) / 2);
+	var section4Top =  $('#sponsorpage').offset().top - (($(document).height() - $('#sponsorpage').offset().top) / 2);;
+	$('nav#primary a').removeClass('active');
+	if($(document).scrollTop() >= section1Top && $(document).scrollTop() < section2Top){
+		$('nav#primary a.manned-flight').addClass('active');
+	} else if ($(document).scrollTop() >= section2Top && $(document).scrollTop() < section3Top){
+		$('nav#primary a.frameless-parachute').addClass('active');
+	} else if ($(document).scrollTop() >= section3Top && $(document).scrollTop() < section4Top){
+		$('nav#primary a.faq').addClass('active');
+	} else if ($(document).scrollTop() >= section4Top){
+		$('nav#primary a.sponsorpage').addClass('active');
+	}
+	
+}
